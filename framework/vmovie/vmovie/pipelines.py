8953559cade883bdef4400351e3c702368422ecc
mudaixiansheng <327216245@qq.com>
2017-11-23 23:01:26 +0800

    日常更新

--- framework/vmovie/vmovie/pipelines.py
+++ framework/vmovie/vmovie/pipelines.py
@@ -16,11 +16,16 @@ class VmoviePipeline(object):
 
 
 class PicsDownloadPipeline(ImagesPipeline):
+    # def file_path(self, request, response=None, info=None):
+    #     image_name = '这里可以自定义图片名'
+    #     return 'full/%s' % (image_name)
+
     def get_media_requests(self, item, info):
         for image_url in item['cover']:
             yield Request(image_url)
 
     def item_completed(self, results, item, info):
+        # [scrapy.pipelines.files] ：https://segmentfault.com/q/1010000008135270
         image_path = [x['path'] for ok, x in results if ok]
         if not image_path:
             raise DropItem("Item contains no images")
