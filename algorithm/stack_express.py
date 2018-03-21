ae3d0718071ccd40341a613e7fd59e1bcc330f62
mudaixiansheng <327216245@qq.com>
2018-03-21 09:33:46 +0800

    日常更新

--- algorithm/stack_express.py
+++ algorithm/stack_express.py
@@ -49,5 +49,32 @@ def infix2postfix(infixexpr):
     return " ".join(postfix_list)
 
 
+def postfix_eval(postfixexpr):
+    oper_stack = Stack()
+    token_list = postfixexpr.split()
+
+    for token in token_list:
+        if token in "0123456789":
+            oper_stack.push(int(token))
+        else:
+            operand2 = oper_stack.pop()
+            operand1 = oper_stack.pop()
+            result = do_math(token, operand1, operand2)
+            oper_stack.push(result)
+    return oper_stack.pop()
+
+
+def do_math(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    else:
+        return op1 - op2
+
+
 print(infix2postfix("A * B + C * D"))
 print(infix2postfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+print(postfix_eval('7 8 + 3 2 + /'))
